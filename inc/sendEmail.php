e98543eea834d09abf332c594d8e86c9717c8b3b
Sai Uday Bhaskar Mudivarty <narenuday@Sais-MacBook-Pro.local>
2017-03-13 13:47:54 -0700

    Initial Commit

---
+++ inc/sendEmail.php
@@ -0,0 +1,88 @@
+﻿<?php
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+// Replace this with your own email address
+$siteOwnersEmail = 'user@website.com';
+
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['contactName']));
+   $email = trim(stripslashes($_POST['contactEmail']));
+   $subject = trim(stripslashes($_POST['contactSubject']));
+   $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+   // Check Name
+	if (strlen($name) < 2) {
+		$error['name'] = "Please enter your name.";
+	}
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address.";
+	}
+	// Check Message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	}
+   // Subject
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+	$message .= "Email address: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= $contact_message;
+   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+   if (!$error) {
+	   $httpClient = new GuzzleAdapter(new Client());
+$sparky = new SparkPost($httpClient, ['key'=>getEnv('SPARKPOST_API_KEY')]);
+
+$sparky->setOptions(['async' => false]);
+$results = $sparky->transmission->post([
+  'options' => [
+    'sandbox' => true
+  ],
+  'content' => [
+    'from'=>'testing@' . getEnv('SPARKPOST_SANDBOX_DOMAIN'),
+    'subject'=> 'Oh hey!',
+    'html'=>'<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>'
+  ],
+  'recipients'=>[
+    ['address'=>['email'=>'developers+php@sparkpost.com']]
+  ]
+]);
+
+      /*ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+		
+	} */# end if - no validation error
+} 
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	} # end if - there was a validation error
+
+}
+
+?>
